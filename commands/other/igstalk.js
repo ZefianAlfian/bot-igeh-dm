472a699afde3b538169cef7a56850a5042564b81
ZefianAlfian <tryaha78@gmail.com>
2022-03-08 21:57:46 +0700

    example igstalk

---
+++ commands/other/igstalk.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: "igstalk",
+  // aliases: ["h"],
+  run: async (conn, m, args) => {
+    let usr = await conn.ig.user.getIdByUsername((args[0]).replace("@", ""));
+    let acc = await conn.ig.user.info(usr);
+    if (acc.is_private){
+    	conn.sendDM(m.sender.pk, "User private");
+    	return;
+    }
+    // console.log(acc)
+    conn.sendDM(m.sender.pk, `Username: ${acc.username}\nFull Name: ${acc.full_name}`)
+  },
+};
